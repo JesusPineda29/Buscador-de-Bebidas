32a8b8b713ab8e663509db35adfe0f339674ee21
JesusPineda29 <jesuspineda1429@gmail.com>
2025-06-16 14:48:23 -0600

    fix: scroll automático a resultados tras búsqueda

--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react"
+import { useEffect, useMemo, useRef } from "react"
 import { useAppStore } from "../stores/useAppStore"
 import { DrinkCard } from "../components/DrinkCard"
 
@@ -7,16 +7,30 @@ import { DrinkCard } from "../components/DrinkCard"
 export const IndexPage = () => {
 
   const drinks = useAppStore((state) => state.drinks)
+  const hasDrinks = useMemo(() => drinks.drinks.length, [drinks])
 
-  const hasDrinks = useMemo(() => drinks.drinks.length,[drinks])
+
+  // 🔥 Este ref marca el inicio de los resultados
+  const resultRef = useRef<HTMLDivElement>(null)
+
+  // 🔥 Este efecto hace scroll cuando aparecen resultados
+  useEffect(() => {
+    if (hasDrinks && resultRef.current) {
+      resultRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' })
+    }
+  }, [hasDrinks]) // se dispara cuando hay resultados
+
+
 
   return (
     <>
       <h1 className="text-6xl font-extrabold">Recetas</h1>
 
+      <div ref={resultRef}></div> {/* 👈 Aquí nos movemos con scroll */}
+
       {hasDrinks ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 my-10 gap-10">
-          {drinks.drinks.map((drink)=> (
+          {drinks.drinks.map((drink) => (
             <DrinkCard
               key={drink.idDrink}
               drink={drink}
