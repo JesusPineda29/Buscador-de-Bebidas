fc10928d1b28f48926d43b0666c18df7dbfc322f
JesusPineda29 <jesuspineda1429@gmail.com>
2025-06-15 18:10:08 -0600

    creando slice de AI

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,14 +1,16 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 import { createRecipesSlice, type RecipesSliceType } from "./recipeSlice";
-import { type FavoritesSliceType, createFavaoritesSlice } from "./favoritesSlice";
+import { createFavaoritesSlice, type FavoritesSliceType } from "./favoritesSlice";
 import { createNotificationSlice, type NotificationSliceType } from "./notificationSlice";
+import { createAISlice, type AISlice } from "./aiSlice";
 
 
-export const useAppStore = create<RecipesSliceType & FavoritesSliceType & NotificationSliceType>()(devtools((...a) => ({
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType & NotificationSliceType & AISlice>()(devtools((...a) => ({
     ...createRecipesSlice(...a),
     ...createFavaoritesSlice(...a),
-    ...createNotificationSlice(...a)
+    ...createNotificationSlice(...a),
+    ...createAISlice(...a)
 })))
 
 
